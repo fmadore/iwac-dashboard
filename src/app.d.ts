a5ea099a83e56f8c06c2d80c47443cd74f292e78
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-10-02 08:59:57 +0200

    feat: Add PWA registration and info type declarations to vite-env.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -46,45 +46,4 @@ declare module 'd3-cloud' {
 	export default function cloud(): CloudLayout;
 }
 
-declare module 'virtual:pwa-register' {
-	export interface RegisterSWOptions {
-		immediate?: boolean;
-		onNeedRefresh?: () => void;
-		onOfflineReady?: () => void;
-		onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-		onRegisterError?: (error: any) => void;
-	}
-
-	export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
-}
-
-declare module 'virtual:pwa-register/svelte' {
-	import type { Writable } from 'svelte/store';
-
-	export interface RegisterSWOptions {
-		immediate?: boolean;
-		onNeedRefresh?: () => void;
-		onOfflineReady?: () => void;
-		onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-		onRegisterError?: (error: any) => void;
-	}
-
-	export function useRegisterSW(options?: RegisterSWOptions): {
-		needRefresh: Writable<boolean>;
-		offlineReady: Writable<boolean>;
-		updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
-	};
-}
-
-declare module 'virtual:pwa-info' {
-	export interface PwaInfo {
-		webManifest: {
-			href: string;
-			linkTag: string;
-		};
-	}
-
-	export const pwaInfo: PwaInfo | undefined;
-}
-
 export {};
