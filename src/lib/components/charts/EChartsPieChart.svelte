8528774a59b4de3215e75ba8bdd9c49d0cc2736d
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-01-04 22:21:45 +0100

    Remove EChartsBarChart and EChartsPieChart components

--- src/lib/components/charts/EChartsPieChart.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import LayerChartPieChart from './LayerChartPieChart.svelte';
-
-	// Backwards-compatible wrapper: this component used to be the ECharts implementation.
-	// Keeping it as an alias avoids breaking any older imports.
-	const props = $props();
-</script>
-
-<LayerChartPieChart {...props} />
